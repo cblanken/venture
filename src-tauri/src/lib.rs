f49eedc83060e9d8602808534ee0d4122fb0cd21
Michael Taggart (UCLA-H) <mtaggart@mednet.ucla.edu>
2024-11-06 14:17:53 -0800

    Working pagination/async

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,37 +1,72 @@
-use tauri::{Builder, Manager};
-use std::path::Path;
+use tauri::{Builder, Manager, State};
+use std::sync::Mutex;
+use serde::Serialize;
 use evtx;
 
+const DEFAULT_PAGE_SIZE: usize = 10;
+
 #[derive(Default)]
-struct AppData {
-    current_page: usize,
-    events: Vec<String>
+struct AppState {
+    events: Mutex<Vec<String>>,
+    page_size: Mutex<usize>
+
+}
+
+#[derive(Default, Debug, Serialize)]
+struct PageResult {
+    events: Vec<String>,
+    page_num: usize,
+    page_size: usize,
+    total_events: usize
+}
+
+#[tauri::command]
+async fn select_page(selected: usize, state: State<'_, AppState>) -> Result<PageResult, ()> {
+    let page_size = *state.page_size.lock().unwrap();
+    let events = state.events.lock().unwrap();
+    let start_idx = (selected - 1) * page_size;
+    let end_idx = start_idx + page_size;
+    let res = PageResult {
+        events: events[start_idx..end_idx].to_vec(),
+        page_num: selected,
+        page_size: page_size,
+        total_events: events.len(),
+    };
+    Ok(res)
 }
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn load_evtx(selected: &Path) -> Vec<String> {
+async fn load_evtx(selected: String, state: State<'_, AppState >) -> Result<PageResult, ()> {
     let mut parser = evtx::EvtxParser::from_path(selected).unwrap();
-    parser.records_json().map(|r| {
+    let events: Vec<String> = parser.records_json().map(|r| {
         r.unwrap().data
     })  
-    .collect()   
+    .collect();
+    *state.events.lock().unwrap() = events.clone();
+    Ok(PageResult {
+        events: events[0..DEFAULT_PAGE_SIZE].to_vec(),
+        page_num: 1,
+        page_size: DEFAULT_PAGE_SIZE,
+        total_events: events.len()
+    })
+
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     Builder::default()
         .setup(|app| {
-            app.manage(AppData {
-                current_page: 0,
-                events: vec![]
+            app.manage(AppState {
+                events: Mutex::new(vec![]),
+                page_size: Mutex::new(DEFAULT_PAGE_SIZE)
             });
             Ok(())
         })
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![load_evtx])
+        .invoke_handler(tauri::generate_handler![load_evtx, select_page])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,35 @@ import EventTable from "./EventTable"
 
 import "./App.css";
 
+const DEFAULT_PAGE_SIZE = 10;
+
+interface PageResult {
+  events: string[],
+  page_num: number,
+  page_size: number,
+  total_events: number
+}
+
 function App() {
   const [selectedFile, setSelectedFile] = useState("");
-  const [eventPage, setEventPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [events, setEvents]: [Object[], Function] = useState([]);
+  const [totalEvents, setTotalEvents]: [number, Function] = useState(0);
+  const [pageSize, setPageSize]: [number, Function] = useState(DEFAULT_PAGE_SIZE);
+
+
+  async function parseEvents(eventStrings: string[]) {
+    return eventStrings.map( (j: string) => {
+      let json = JSON.parse(j);
+      return Object.assign({}, json.Event.EventData, json.Event.System);
+    });
+  }
+
+  async function getPage(selected: number) {
+    let res: PageResult = await invoke("select_page", { selected });
+    setCurrentPage(res.page_num);
+    setEvents(await parseEvents(res.events));
+  }
 
   async function getFile() {
     const selected = await open({
@@ -23,14 +48,13 @@ function App() {
       }]
     });
     console.log(selected);
-    let records: string[] = await invoke("load_evtx", { selected });
-    let parsed: Object[] = records.map( (j: string) => {
-      
-      let json = JSON.parse(j);
-      return Object.assign({}, json.Event.EventData, json.Event.System);
-    });
-    console.log(parsed);
-    setEvents(parsed);
+    let res: PageResult = await invoke("load_evtx", { selected });
+    let events: Object[] = await parseEvents(res.events);
+    console.log(events);
+    setCurrentPage(res.page_num);
+    setTotalEvents(res.total_events);
+    setPageSize(res.page_size);
+    setEvents(events);
   }
 
   return (
@@ -39,7 +63,28 @@ function App() {
       <button type="button" onClick={async () =>{await getFile()}}>Open</button>
       {
         events.length > 0 ?
-        <EventTable events={events}></EventTable>
+        <>
+          <EventTable events={events}></EventTable>
+          <div className="paginator">
+            <p>
+              <button 
+                className="paginate prev" 
+                disabled={currentPage == 1} 
+                onClick={() => getPage(currentPage - 1)}
+              >
+                &lt; 
+              </button>
+              Page {currentPage} of { Math.ceil(totalEvents / pageSize) } 
+              <button 
+                className="paginate next" 
+                disabled={currentPage === (Math.ceil(totalEvents / pageSize))} 
+                onClick={() => getPage(currentPage + 1)}
+              > 
+                &gt;
+              </button>
+            </p>
+          </div>
+        </>
         : null
       }
     </main>
